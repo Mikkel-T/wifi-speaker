e635b55666accfa9a0f0b3d0556f6d980a2f54e0
Mikkel-T <mikkel@mikkel-t.com>
2024-02-27 15:27:06 +0100

    Change default bind address

--- src/main.rs
+++ src/main.rs
@@ -148,7 +148,7 @@ async fn main() -> std::io::Result<()> {
             .service(upload)
             .service(queue)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
